b76e9100a0405bb738ceb9cf3c41b41e47543427
Dinara <inraidi@gmail.com>
2021-01-19 11:13:19 -0500

    Setting the controllers and transparency transformation

---
+++ Assets/Classwork_Jan19_DS/TransparencyChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparencyChanger : MonoBehaviour
+{
+    public Material material;
+    public void UpdateTransparency(float alpha)
+    {
+       
+        Color color = material.color;
+        color.a = alpha;
+        material.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
