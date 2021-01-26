82b7d806827bfae8fa0c5e7396cfaf67840dc177
Dinara <inraidi@gmail.com>
2021-01-26 09:53:21 -0500

    Transparency changes [2]

---
+++ Assets/Classworks/Classwork_Jan19_DS/TransparencyChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparencyChanger : MonoBehaviour
+{
+    public Material material;
+    public void UpdateTransparencyByBool(bool opaque)
+    {
+
+        Color color = material.color;
+        if (opaque)
+              { color.a = 1f; }
+        else { color.a = 0f; }
+        material.color = color;
+    }
+
+    public void UpdateTransparency(float alpha)
+    {
+
+        if (alpha >= 0)
+        {
+            print("alpha" + alpha);
+            Color color = material.color;
+            color.a = alpha;
+            material.color = color;
+        }
+    }
+
+
+}
